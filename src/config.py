eb1e33662b698b3a5ce84b19f6de9a80bdd5a536
Damaris Blas <damarisblas123@gmail.com>
2025-12-04 17:54:28 -0400

    arreglar config

--- src/config.py
+++ src/config.py
@@ -1,7 +1,17 @@
 ﻿import os
 
+db_url = os.getenv(
+    'DATABASE_URL',
+    'postgresql://postgres:root@localhost:5432/escuela0'
+)
+print(">>> DATABASE_URL en tiempo de ejecución:", db_url)
+
+
+if db_url and db_url.startswith("postgres://"):
+    db_url = db_url.replace("postgres://", "postgresql+psycopg2://", 1)
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://postgres:root@localhost:5432/escuela0')
+    SQLALCHEMY_DATABASE_URI = db_url
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'abcd'
     UPLOAD_FOLDER = os.path.join(os.getcwd(), 'uploads')
